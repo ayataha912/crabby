4cbefe457fc2f55a36b650cbf4639a0096bd468f
Anas Ghareib <AnasGhareib@gmail.com>
2023-07-24 19:30:36 +0300

    creating a temp converter

---
+++ tmp_converter/target/debug/deps/tmp_converter-c09eea305ba858dd.d
@@ -0,0 +1,5 @@
+/home/aghthar/Desktop/crabby/tmp_converter/target/debug/deps/tmp_converter-c09eea305ba858dd.rmeta: src/main.rs
+
+/home/aghthar/Desktop/crabby/tmp_converter/target/debug/deps/tmp_converter-c09eea305ba858dd.d: src/main.rs
+
+src/main.rs:
