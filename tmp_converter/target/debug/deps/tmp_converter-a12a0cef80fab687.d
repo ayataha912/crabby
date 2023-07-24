5c8a532ebd1d99266b7a4cf40e78f52c24b62c2c
Anas Ghareib <AnasGhareib@gmail.com>
2023-07-24 19:35:04 +0300

    Ignore target directory

--- tmp_converter/target/debug/deps/tmp_converter-a12a0cef80fab687.d
+++
@@ -1,5 +0,0 @@
-/home/aghthar/Desktop/crabby/tmp_converter/target/debug/deps/tmp_converter-a12a0cef80fab687.rmeta: src/main.rs
-
-/home/aghthar/Desktop/crabby/tmp_converter/target/debug/deps/tmp_converter-a12a0cef80fab687.d: src/main.rs
-
-src/main.rs:
--- tmp_converter/target/debug/deps/tmp_converter-c09eea305ba858dd.d
+++
@@ -1,5 +0,0 @@
-/home/aghthar/Desktop/crabby/tmp_converter/target/debug/deps/tmp_converter-c09eea305ba858dd.rmeta: src/main.rs
-
-/home/aghthar/Desktop/crabby/tmp_converter/target/debug/deps/tmp_converter-c09eea305ba858dd.d: src/main.rs
-
-src/main.rs:
